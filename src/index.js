192b41043ef093b0d2bfeb41087e01445093b8da
Josh Taylor <joshtaylor.se@gmail.com>
2022-06-10 17:44:00 -0400

    Clean up and add geocoding api to position map centered on Ontario.

--- src/index.js
+++ src/index.js
@@ -1,84 +1,95 @@
 // Function to request a list of object id's from the government Ontario website.
-async function getObjectIds(){
+async function getObjectIds() {
 	let response = await fetch('https://ws.lioservices.lrc.gov.on.ca/arcgis1071a/rest/services/LIO_OPEN_DATA/LIO_Open06/MapServer/5/query?' + new URLSearchParams({
-  f: 'json',
-  outSR: '4326',
-  returnIdsOnly: true,
-  where: '1=1'
-  }));
-  let data = await response.json()
-	
+		f: 'json',
+		outSR: '4326',
+		returnIdsOnly: true,
+		where: '1=1'
+	}));
+	let data = await response.json()
+
 	// Too much data. Return a chunk for now.
 	console.log(`Total object id's found: ${data.objectIds.length}`);
-  return data.objectIds.slice(0, 50);
+	return data.objectIds.slice(0, 50);
 }
 
 // Function to request specific object data from the government Ontario website.
-async function getData(objectIds){
+async function getData(objectIds) {
 	let response = await fetch('https://ws.lioservices.lrc.gov.on.ca/arcgis1071a/rest/services/LIO_OPEN_DATA/LIO_Open06/MapServer/5/query?' + new URLSearchParams({
-  f: 'json',
-  outSR: '4326',
-  objectIds: objectIds,
-  outFields: ["*"],
-  returnGeometry: true,
-  where: '1=1'
-  }));
-  let data = await response.json()
-  return data;
+		f: 'json',
+		outSR: '4326',
+		objectIds: objectIds,
+		outFields: ["*"],
+		returnGeometry: true,
+		where: '1=1'
+	}));
+	let data = await response.json()
+	return data;
 }
 
 // Function to add logo to the map with link to website.
 function MyLogoControl(controlDiv) {
-    controlDiv.style.padding = '5px';
-    var logo = document.createElement('IMG');
-    logo.src = '../img/logo.png';
-    logo.style.cursor = 'pointer';
+	controlDiv.style.padding = '5px';
+	var logo = document.createElement('IMG');
+	logo.src = '../img/logo.png';
+	logo.style.cursor = 'pointer';
 	logo.s
-    controlDiv.appendChild(logo);
+	controlDiv.appendChild(logo);
 
-    google.maps.event.addDomListener(logo, 'click', function() {
-        window.location = 'https://www.citizencrown.ca/'; 
-    });
+	logo.addEventListener('click', function () {
+		window.location = 'https://www.citizencrown.ca/';
+	});
 }
 
 // This example creates simple polygons representing crown land in Ontario.
 function initMap() {
 	const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 6,
-    center: { lat: 49.000, lng: -80.000},
-    mapTypeId: "terrain",
-	disableDefaultUI: true,
-  });
+		zoom: 6,
+		mapTypeId: "terrain",
+		disableDefaultUI: true,
+	});
 	console.log("Initializing map.");
 
-  // Add logo control.
-  const logoControlDiv = document.createElement("div");
-  MyLogoControl(logoControlDiv)
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(logoControlDiv);
+	// Focus map position Ontario.
+	const geocoder = new google.maps.Geocoder();
+	geocoder
+		.geocode({ address: "Ontario" })
+		.then((response) => {
+			const position = response.results[0].geometry.location;
+			map.setCenter(position);
+		})
+		.catch((e) =>
+			window.alert("Geocode was not successful for the following reason: " + e)
+		);
 
-  // Get a list of object id's.
-  getObjectIds().then(ids => {
+	// Add logo control.
+	const logoControlDiv = document.createElement("div");
+	MyLogoControl(logoControlDiv)
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(logoControlDiv);
+
+	// Get a list of object id's.
+	getObjectIds().then(ids => {
 		console.log(`Fetched ids: ${ids}`);
-		
+
 		// Get data and log it to console.
 		getData(ids.toString()).then(data => {
 			console.log(`Fetched ${ids.length} data objects.`);
 			console.debug(data);
-			
+
 			// Iterate through each features geometry set.
 			data.features.forEach(feature => {
-				
+
 				// Create coord paths for map.
 				var rings = feature.geometry.rings;
 				var paths = new Array(rings.length);
 				rings.forEach((group, group_index) => {
 					paths[group_index] = new Array(group.length);
 					group.forEach((region, region_index) => {
-						paths[group_index][region_index] = { lng: region[0], lat: region[1]};
+						paths[group_index][region_index] = { lng: region[0], lat: region[1] };
 					})
 				})
 				console.log(`Created paths for: [${feature.attributes.OBJECTID}] ${feature.attributes.NAME_ENG} (${feature.attributes.DESIGNATION_ENG})`);
-				
+
 				// Construct the polygon.
 				const polygonArea = new google.maps.Polygon({
 					paths: paths,
@@ -93,5 +104,5 @@ function initMap() {
 				console.log(`Created polygon(s) for: [${feature.attributes.OBJECTID}] ${feature.attributes.NAME_ENG} (${feature.attributes.DESIGNATION_ENG})`);
 			})
 		});
-  });
+	});
 }
